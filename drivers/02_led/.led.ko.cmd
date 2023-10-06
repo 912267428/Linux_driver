25e25f5001e08620ec77f42c4388c285b831d339
912267428 <912267428@qq.com>
2023-10-06 14:36:00 +0800

    led

---
+++ drivers/02_led/.led.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rodney/Desktop/Linux_driver/drivers/02_led/led.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/rodney/Desktop/Linux_driver/drivers/02_led/led.ko /home/rodney/Desktop/Linux_driver/drivers/02_led/led.o /home/rodney/Desktop/Linux_driver/drivers/02_led/led.mod.o
