c744074bd1f8c35d75e26983f3a24a6b75375770
912267428 <912267428@qq.com>
2023-10-06 17:26:39 +0800

    newchrled

---
+++ drivers/03_newchrled/.newled.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.ko /home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.o /home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.mod.o
---
+++ drivers/03_newchrled/.tmp_versions/newled.mod
@@ -0,0 +1,2 @@
+/home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.ko
+/home/rodney/Desktop/Linux_driver/drivers/03_newchrled/newled.o
