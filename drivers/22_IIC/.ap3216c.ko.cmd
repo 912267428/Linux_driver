fdacfca1538520c0488c4cd4ac1f4310fedd3275
912267428 <912267428@qq.com>
2023-11-02 13:59:23 +0800

    ii2

---
+++ drivers/22_IIC/.ap3216c.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rodney/Desktop/Linux_driver/drivers/22_IIC/ap3216c.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/rodney/Desktop/Linux_driver/drivers/22_IIC/ap3216c.ko /home/rodney/Desktop/Linux_driver/drivers/22_IIC/ap3216c.o /home/rodney/Desktop/Linux_driver/drivers/22_IIC/ap3216c.mod.o
