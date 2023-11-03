86d01e2e6736b91b25b17ce2ecfd469d4370f510
912267428 <912267428@qq.com>
2023-11-03 15:05:27 +0800

    spi

---
+++ drivers/23_spi/.icm20608.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.ko /home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.o /home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.mod.o
---
+++ drivers/23_spi/.tmp_versions/icm20608.mod
@@ -0,0 +1,2 @@
+/home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.ko
+/home/rodney/Desktop/Linux_driver/drivers/23_spi/icm20608.o
